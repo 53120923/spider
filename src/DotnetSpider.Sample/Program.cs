e70afbf52757246464946c13bd2cd43beb31b747
53120923 <2264810419@qq.com>
2017-06-17 08:57:02 +0800

    Create Program.cs

---
+++ src/DotnetSpider.Sample/Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotnetSpider.Sample
+{
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CustomSpider1 s = new CustomSpider1();
+			s.Run();
+			//ConfigurableSpider.Run();
+
+			// Custmize processor and pipeline 完全自定义页面解析和数据管道
+			BaseUsage.CustmizeProcessorAndPipeline();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			// Crawler pages without traverse 采集指定页面不做遍历
+			BaseUsage.CrawlerPagesWithoutTraverse();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			// Crawler pages traversal 遍历整站
+			BaseUsage.CrawlerPagesTraversal();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			DDengEntitySpider dDengEntitySpider = new DDengEntitySpider();
+			dDengEntitySpider.Run();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			Cnblogs.Run();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			CasSpider casSpider = new CasSpider();
+			casSpider.Run();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			BaiduSearchSpider baiduSearchSpider = new BaiduSearchSpider();
+			baiduSearchSpider.Run();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			JdSkuSampleSpider jdSkuSampleSpider = new JdSkuSampleSpider();
+			jdSkuSampleSpider.Run();
+			Console.WriteLine("Press any key to continue...");
+			Console.Read();
+
+			Situoli.Run();
+		}
+	}
+}
